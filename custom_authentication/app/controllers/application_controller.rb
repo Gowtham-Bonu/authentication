8c75f666cbe2db2e4b6c3bb97ec0f83d63744fa2
gowtham-bonu-simform <gowtham@simformsolutions.com>
2023-05-18 13:34:06 +0530

    TE7-T129:UserAdvanceAuthenticationInRails:Feat:ref

--- custom_authentication/app/controllers/application_controller.rb
+++ custom_authentication/app/controllers/application_controller.rb
@@ -10,13 +10,13 @@ class ApplicationController < ActionController::Base
   end
 
   def current_user
-    if (user_id = session[:user_id])
-      @current_user ||= User.find_by(id: user_id)
+    @current_user ||= if (user_id = session[:user_id])
+      User.find_by(id: user_id)
     elsif (user_id = cookies.signed[:user_id])
       user = User.find_by(id: user_id)
       if user && user.authenticated?(cookies[:remember_digest])
         log_in user
-        @current_user = user
+        user
       end
     end
   end
